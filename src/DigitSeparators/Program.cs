c915febf0410e92f1ed7fcc7a5112f269a5a9d81
Leonardo Pereira de Jesus <leonardo.pereira@keeggo.com>
2022-10-19 19:02:41 -0300

    Novos recursos

---
+++ src/DigitSeparators/Program.cs
@@ -0,0 +1,12 @@
+﻿const int LETRA_Y_BIN = 0b01_01_10_01; //Definição de binário (01011001) = 89 (decimal)
+const int LETRA_Z_HEX = 0X5_A; //Definição hexadecimal, 5A (hexadecimal) = 80 (decimal)
+const int POPULACAO_ESTIMADA_BRASIL = 204_500_000;
+const double RENDA_PER_CAPITA_BRASIL_USD = 11_208;
+
+Console.WriteLine("Valor numérico");
+Console.WriteLine($"Y = {LETRA_Y_BIN}");
+Console.WriteLine($"Z = {LETRA_Z_HEX}");
+Console.WriteLine(string.Empty);
+Console.WriteLine($"População estimada do Brasil = {POPULACAO_ESTIMADA_BRASIL}");
+Console.WriteLine($"Renda per capta do Brasil (US$) = {RENDA_PER_CAPITA_BRASIL_USD}");
+Console.ReadKey();
---
+++ src/LocalFunctions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Local function utilizado para criação de métodos dentro de outro método, não sendo exposto na camada externa, usabilidade recomendada em casos de metódos que precisam ser chamadas diversas vezes em um unico lugar e não precisa expor aos outros
+            string Inverter(string valor) 
+            {
+                return new string(
+                    valor.ToCharArray().Reverse().ToArray());
+            }
+
+            string texto;
+
+            texto = "Utilizando o C#";
+            Console.WriteLine($"{texto} -> {Inverter(texto)}");
+
+            texto = "Local Function";
+            Console.WriteLine($"{texto} -> {Inverter(texto)}");
+
+            Console.ReadKey();
+        }
+    }
+}
